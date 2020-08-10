3eff6a68fc02e40c05d39333a4c33d765877207f
Asawari Joshi <asawari44@gmail.com>
2020-08-10 21:18:36 +0300

    changed icon

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -1,6 +1,6 @@
 import makeFAIcon from "utils/makeFAIcon";
 
-import { faLaptop, faCloud, faHandshake } from "@fortawesome/free-solid-svg-icons";
+import { faMobile, faCogs, faHandshake } from "@fortawesome/free-solid-svg-icons";
 
 export const ShoppingCartIcon = makeFAIcon(faMobile);
 export const LaptopIcon = makeFAIcon(faCogs);
