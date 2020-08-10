70131b44fc38c7580805c85639d74d9b1fbfcb40
Asawari Joshi <asawari44@gmail.com>
2020-08-10 20:50:54 +0300

    services

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -2,6 +2,6 @@ import makeFAIcon from "utils/makeFAIcon";
 
 import { faLaptop, faCloud, faHandshake } from "@fortawesome/free-solid-svg-icons";
 
-export const ShoppingCartIcon = makeFAIcon(faShoppingCart);
+export const ShoppingCartIcon = makeFAIcon(faCloud);
 export const LaptopIcon = makeFAIcon(faLaptop);
-export const LockIcon = makeFAIcon(faLock);
+export const LockIcon = makeFAIcon(faHandshake);
